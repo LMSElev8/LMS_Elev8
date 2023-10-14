2450d02f59e9602a02a58b31be483d1e84e7f86d
MuratSklzn <muratsakaliuzun@hotmail.com>
2023-10-14 16:53:03 +0300

    enrollment
    view ve controller
    controllerda tablonun verisini değştirdim idyi username yaptım

---
+++ LMS_Project/Controllers/EnrollmentsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LMS_Project.Data;
+using LMS_Project.Models;
+
+namespace LMS_Project.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EnrollmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Enrollments
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Enrollments.Include(e => e.Course).Include(e => e.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Enrollments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Enrollments == null)
+            {
+                return NotFound();
+            }
+
+            var enrollment = await _context.Enrollments
+                .Include(e => e.Course)
+                .Include(e => e.User)
+                .FirstOrDefaultAsync(m => m.UniqueId == id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserName", "UserName");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UniqueId,UserId,CourseId,EnrollmentDate")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(enrollment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", enrollment.CourseId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", enrollment.UserId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Enrollments == null)
+            {
+                return NotFound();
+            }
+
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", enrollment.CourseId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", enrollment.UserId);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UniqueId,UserId,CourseId,EnrollmentDate")] Enrollment enrollment)
+        {
+            if (id != enrollment.UniqueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(enrollment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EnrollmentExists(enrollment.UniqueId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", enrollment.CourseId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", enrollment.UserId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Enrollments == null)
+            {
+                return NotFound();
+            }
+
+            var enrollment = await _context.Enrollments
+                .Include(e => e.Course)
+                .Include(e => e.User)
+                .FirstOrDefaultAsync(m => m.UniqueId == id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Enrollments == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Enrollments'  is null.");
+            }
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment != null)
+            {
+                _context.Enrollments.Remove(enrollment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EnrollmentExists(int id)
+        {
+          return (_context.Enrollments?.Any(e => e.UniqueId == id)).GetValueOrDefault();
+        }
+    }
+}
