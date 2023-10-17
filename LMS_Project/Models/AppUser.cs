235b574ca32cd321a9670470095cc505bff21b0d
Hüseyin Hilmi Tüllü <hhilmitullu@gmail.com>
2023-10-17 22:37:42 +0300

    düzeltme

--- LMS_Project/Models/AppUser.cs
+++ LMS_Project/Models/AppUser.cs
@@ -12,6 +12,6 @@ namespace LMS_Project.Models
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
 
-        public ICollection<Enrollment>? Enrollments { get; set; }
+        
     }
 }
